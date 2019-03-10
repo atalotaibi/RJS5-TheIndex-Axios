55d1392d23b2da18a5288fd5d6f255110fb13c32
atalotaibi <alotaibiatallah@gmail.com>
2019-03-10 14:18:38 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-
-import authors from "./data.js";
+import axios from "axios";
 
 // Components
 import Sidebar from "./Sidebar";
@@ -10,16 +9,23 @@ import AuthorDetail from "./AuthorDetail";
 class App extends Component {
   state = {
     currentAuthor: null,
-    filteredAuthors: authors
+    filteredAuthors: [],
+    authors: [],
+    loading: true
   };
 
-  selectAuthor = author => this.setState({ currentAuthor: author });
+  selectAuthor = async author => {
+    const response1 = await axios.get(
+      "https://the-index-api.herokuapp.com/api/authors/" + author.id
+    );
+    this.setState({ currentAuthor: response1.data });
+  };
 
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
   filterAuthors = query => {
     query = query.toLowerCase();
-    let filteredAuthors = authors.filter(author => {
+    let filteredAuthors = this.state.authors.filter(author => {
       return `${author.first_name} ${author.last_name}`
         .toLowerCase()
         .includes(query);
@@ -33,7 +39,7 @@ class App extends Component {
     } else {
       return (
         <AuthorsList
-          authors={this.state.filteredAuthors}
+          authors={this.state.authors}
           selectAuthor={this.selectAuthor}
           filterAuthors={this.filterAuthors}
         />
@@ -41,7 +47,20 @@ class App extends Component {
     }
   };
 
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        "https://the-index-api.herokuapp.com/api/authors/"
+      );
+      const authors = response.data;
+      this.setState({ authors: authors, loading: false });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
+    if (this.state.loading) return <div>loading ...</div>;
     return (
       <div id="app" className="container-fluid">
         <div className="row">
